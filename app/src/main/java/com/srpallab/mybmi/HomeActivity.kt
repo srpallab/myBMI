8d8b0f7f33c26dd45efed24fb092a046481bc86b
srpallab <pallab.alif@gmail.com>
2023-01-04 02:32:44 +0600

    UI Fixed.

--- app/src/main/java/com/srpallab/mybmi/HomeActivity.kt
+++ app/src/main/java/com/srpallab/mybmi/HomeActivity.kt
@@ -2,10 +2,21 @@ package com.srpallab.mybmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.google.android.material.textfield.TextInputEditText
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var etWeight: TextInputEditText
+    private lateinit var etHeight: TextInputEditText
+    private lateinit var btnCalculate : ImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        etWeight = findViewById(R.id.etWeight)
+        etHeight = findViewById(R.id.etHeight)
+        btnCalculate = findViewById(R.id.btnCalculate)
+
+
     }
 }
